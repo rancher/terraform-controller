761ffe661adacead626f1f354f6072c5364e841b
Dan Ramich <danold215@gmail.com>
2019-02-08 10:44:57 -0700

    Support envvars

--- pkg/controllers/execution/execution.go
+++ pkg/controllers/execution/execution.go
@@ -2,13 +2,17 @@ package execution
 
 import (
 	"context"
-	"fmt"
+	"time"
 
 	batchv1 "github.com/ibuildthecloud/terraform-operator/types/apis/batch/v1"
 	"github.com/ibuildthecloud/terraform-operator/types/apis/client"
 	coreclient "github.com/ibuildthecloud/terraform-operator/types/apis/core/v1"
 	rbacv1 "github.com/ibuildthecloud/terraform-operator/types/apis/rbac.authorization.k8s.io/v1"
 	"github.com/ibuildthecloud/terraform-operator/types/apis/terraform-operator.cattle.io/v1"
+	"github.com/pkg/errors"
+	"github.com/rancher/norman/controller"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
@@ -35,7 +39,7 @@ func Register(ctx context.Context, ns string, client *client.MasterClient) error
 		serviceAccounts:     client.Core.ServiceAccounts(""),
 	}
 
-	client.Operator.Executions(ns).AddLifecycle(ctx, "execution-controller", e)
+	client.Operator.Executions("").AddLifecycle(ctx, "execution-controller", e)
 	return nil
 }
 
@@ -55,29 +59,93 @@ type executionLifecycle struct {
 }
 
 func (e *executionLifecycle) Create(obj *v1.Execution) (runtime.Object, error) {
-	fmt.Println("Create")
 	if obj.Spec.Version < 1 {
 		obj.Spec.Version = 1
+	}
+	if obj.Spec.Image == "" {
+		// TODO: Need a real default image
+		obj.Spec.Image = "dramich/terraform-executor:dev"
 	}
 	return e.executions.Update(obj)
 }
 
 func (e *executionLifecycle) Remove(obj *v1.Execution) (runtime.Object, error) {
 	input, ok, err := e.gatherInput(obj)
-	if !ok || err != nil {
-		fmt.Printf("gettin here: %v, %v", ok, err)
+	if !ok {
+		v1.ExecutionConditionMissingInfo.True(obj)
+		return e.executions.Update(obj)
+	}
+	if err != nil {
 		return obj, err
 	}
 
-	return obj, e.deployDestroy(obj, input, ActionDestroy)
+	v1.ExecutionConditionMissingInfo.False(obj)
+
+	if obj.Spec.DestroyOnDelete {
+		var runName string
+		_, err = v1.ExecutionConditionDestroyJobDeployed.DoUntilTrue(obj, func() (runtime.Object, error) {
+			runName, err = e.deployDestroy(obj, input, ActionDestroy)
+			if err != nil {
+				return obj, err
+			}
+
+			if obj.Status.ExecutionRunName != runName {
+				obj.Status.ExecutionRunName = runName
+				obj, err = e.executions.Update(obj)
+				if err != nil {
+					return obj, errors.WithMessage(err, "updating executionRun on execution")
+				}
+			}
+			return obj, nil
+		})
+		if err != nil {
+			return obj, errors.WithMessage(err, "track error")
+		}
+
+		if runName == "" {
+			combinedVars := combineVars(input)
+			combinedVars["key"] = obj.Name
+			name := createExecRunAndSecretName(obj, combinedVars, input.Module.Status.ContentHash)
+			runName = name + "-destroy"
+		}
+
+		run, err := e.executionRunLister.Get(obj.Namespace, runName)
+		if err != nil {
+			return obj, errors.WithMessage(err, "error getting execution run")
+		}
+
+		if v1.ExecutionRunConditionApplied.IsTrue(run) {
+			err = e.executionRuns.DeleteNamespaced(run.Namespace, run.Name, &metaV1.DeleteOptions{})
+			if err != nil {
+				if !k8serrors.IsNotFound(err) {
+					return obj, err
+				}
+			}
+			return obj, nil
+		}
+
+		if !v1.ExecutionConditionWatchRunning.IsTrue(obj) {
+			go e.watchDestroyRun(obj, run)
+			v1.ExecutionConditionWatchRunning.True(obj)
+		}
+
+		return obj, &controller.ForgetError{}
+
+	}
+	return obj, nil
 }
 
 func (e *executionLifecycle) Updated(obj *v1.Execution) (runtime.Object, error) {
 	input, ok, err := e.gatherInput(obj)
-	if !ok || err != nil {
-		fmt.Printf("gettin here: %v, %v", ok, err)
+	if !ok {
+		v1.ExecutionConditionMissingInfo.True(obj)
+		return e.executions.Update(obj)
+	}
+	if err != nil {
 		return obj, err
 	}
+
+	v1.ExecutionConditionMissingInfo.False(obj)
 
 	return v1.ExecutionConditionJobDeployed.Track(obj, e.executions, func() (runtime.Object, error) {
 		runName, err := e.deployCreate(obj, input, ActionCreate)
@@ -86,11 +154,26 @@ func (e *executionLifecycle) Updated(obj *v1.Execution) (runtime.Object, error) 
 		}
 
 		if obj.Status.ExecutionRunName != runName {
-			obj = obj.DeepCopy()
 			obj.Status.ExecutionRunName = runName
 			return e.executions.Update(obj)
 		}
 
 		return obj, nil
 	})
+}
+
+// watchDestroyRun checks the executionRun for the Applied condition, once set
+// terraform destroy was run so the execution can be requeued for deletion.
+func (e *executionLifecycle) watchDestroyRun(execution *v1.Execution, run *v1.ExecutionRun) {
+	for {
+		r, err := e.executionRunLister.Get(run.Namespace, run.Name)
+		if err != nil {
+			return
+		}
+		if v1.ExecutionRunConditionApplied.IsTrue(r) {
+			e.executions.Controller().Enqueue(execution.Namespace, execution.Name)
+			return
+		}
+		time.Sleep(2 * time.Second)
+	}
 }
