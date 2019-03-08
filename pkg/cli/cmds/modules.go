34169820648442285d7a8c7602912c76b150555d
Bill Maxwell <bill@rancher.com>
2019-03-08 14:59:12 -0700

    Stub of CLI

---
+++ pkg/cli/cmds/modules.go
@@ -0,0 +1,145 @@
+package cmds
+
+import (
+	"github.com/rancher/terraform-operator/types/apis/terraform-operator.cattle.io/v1"
+	"github.com/urfave/cli"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var simpleModuleTableHeader = []string{"NAME", "GIT REPO"}
+
+type InvalidArgs struct {}
+
+func (e InvalidArgs) Error() string {
+	return "Invalid args"
+}
+
+func ModuleCommand() cli.Command {
+	return cli.Command{
+		Name: "modules",
+		Aliases: []string{"module"},
+		Usage: "Operations on TF Operator modules",
+		Action: moduleList,
+		Subcommands: []cli.Command{
+			{
+				Name: "ls",
+				Usage: "List Modules",
+				ArgsUsage: "None",
+				Action: moduleList,
+			},
+			{
+				Name: "create",
+				Usage: "Create new module",
+				ArgsUsage: "[NAME] [GIT URL]",
+				Action: createModule,
+			},
+			{
+				Name: "delete",
+				Usage: "Create new module",
+				ArgsUsage: "[NAME]",
+				Action: deleteModule,
+			},
+		},
+
+		
+	}
+}
+
+func moduleList(c *cli.Context) error {
+	kubeConfig := c.GlobalString("kubeconfig")
+	namespace := c.GlobalString("namespace")
+
+	modules, err := getModuleList(namespace, kubeConfig)
+	if err != nil {
+		return err
+	}
+
+	NewTableWriter(getSimpleModuleTableHeader(),moduleListToTableStrings(modules)).Write()
+
+	return nil
+}
+
+func createModule(c *cli.Context) error {
+	kubeConfig := c.GlobalString("kubeconfig")
+	namespace := c.GlobalString("namespace")
+
+	if len(c.Args()) != 2 {
+		return InvalidArgs{}
+	}
+
+	moduleName := c.Args()[0]
+	gitUrl := c.Args()[1]
+
+	return doModuleCreate(namespace,kubeConfig, moduleName, gitUrl)
+}
+
+func deleteModule(c *cli.Context) error {
+	kubeConfig := c.GlobalString("kubeconfig")
+	namespace := c.GlobalString("namespace")
+
+	if len(c.Args()) != 1 {
+		return InvalidArgs{}
+	}
+
+	moduleName := c.Args()[0]
+
+	return doModuleDelete(namespace, kubeConfig, moduleName)
+}
+
+func getModuleList(namespace, kubeConfig string) (*v1.ModuleList, error) {
+	clientSet, err := newModuleClient(kubeConfig)
+	if err != nil {
+		return nil, err
+	}
+	return clientSet.Module.List(namespace ,metav1.ListOptions{})
+
+}
+
+func doModuleCreate(namespace,kubeConfig,name,url string) error {
+	clientSet, err := newModuleClient(kubeConfig)
+	if err != nil {
+		return err
+	}
+
+	module := &v1.Module{
+		Spec: v1.ModuleSpec{
+			ModuleContent: v1.ModuleContent{
+				Git: v1.GitLocation{
+					URL: url,
+				},
+			},
+		},
+	}
+
+	module.Namespace = namespace
+	module.Name = name
+
+	_, err = clientSet.Module.Create(module)
+	return err
+}
+
+func doModuleDelete(namespace,kubeConfig,name string) error {
+	clientSet, err := newModuleClient(kubeConfig)
+	if err != nil {
+		return err
+	}
+
+	return clientSet.Module.Delete(namespace, name, &metav1.DeleteOptions{})
+}
+
+func getSimpleModuleTableHeader() []string {
+  return simpleModuleTableHeader
+}
+
+func moduleListToTableStrings(modules *v1.ModuleList) [][]string {
+	var values [][]string
+
+	for _, module := range modules.Items {
+		values = append(values, []string{
+			module.Name,
+			module.Spec.Git.URL,
+		})
+	}
+
+	return values
+}
---
+++ pkg/cli/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+	"github.com/rancher/terraform-operator/pkg/cli/cmds"
+)
+
+var (
+	VERSION = "v0.0.0-dev"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "rtoc"
+	app.Version = VERSION
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "kubeconfig",
+			EnvVar: "KUBECONFIG",
+			Value:  "${HOME}/.kube/config",
+		},
+		cli.StringFlag{
+			Name:   "namespace,n",
+			EnvVar: "NAMESPACE",
+			Value:  "default",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		cmds.ModuleCommand(),
+	}
+	app.Action = run
+
+	if err := app.Run(os.Args); err != nil {
+		logrus.Fatal(err)
+	}
+}
+
+func run(c *cli.Context) error {
+
+	return nil
+}
