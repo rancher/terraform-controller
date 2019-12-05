a547e2f1ad7b55f5ce9fda508be01262a191aa99
Chris Kim <oats87g@gmail.com>
2019-12-04 16:12:09 -0800

    remove bad unnecessary messages

--- pkg/terraform/execution/handler.go
+++ pkg/terraform/execution/handler.go
@@ -3,9 +3,8 @@ package execution
 import (
 	"context"
 
-	v1 "github.com/rancher/terraform-controller/pkg/apis/terraformcontroller.cattle.io/v1"
+	"github.com/rancher/terraform-controller/pkg/apis/terraformcontroller.cattle.io/v1"
 	tfv1 "github.com/rancher/terraform-controller/pkg/generated/controllers/terraformcontroller.cattle.io/v1"
-	"github.com/sirupsen/logrus"
 )
 
 func NewHandler(ctx context.Context, executions tfv1.ExecutionController, states tfv1.StateController, modules tfv1.ModuleController) *handler {
@@ -25,7 +24,6 @@ type handler struct {
 }
 
 func (h *handler) OnChange(key string, execution *v1.Execution) (*v1.Execution, error) {
-	logrus.Debug("Execution On Change")
 	if execution == nil {
 		return nil, nil
 	}
@@ -36,7 +34,5 @@ func (h *handler) OnChange(key string, execution *v1.Execution) (*v1.Execution, 
 }
 
 func (h *handler) OnRemove(key string, execution *v1.Execution) (*v1.Execution, error) {
-	logrus.Debug("Execution On Remove")
-
 	return execution, nil
 }
