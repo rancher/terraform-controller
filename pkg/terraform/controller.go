ed986a49859c5faaa95b8118a3c479c15e4381bf
Dinar Valeev <dinar.valeev@absa.africa>
2020-10-13 09:53:26 +0200

    Watch for state related resources
    * Enqueue state if Secret or Configmaps holding TF vars are changed
    
    Signed-off-by: Dinar Valeev <dinar.valeev@absa.africa>

--- pkg/terraform/controller.go
+++ pkg/terraform/controller.go
@@ -10,6 +10,10 @@ import (
 	batchv1 "github.com/rancher/wrangler/pkg/generated/controllers/batch/v1"
 	corev1 "github.com/rancher/wrangler/pkg/generated/controllers/core/v1"
 	rbacv1 "github.com/rancher/wrangler/pkg/generated/controllers/rbac/v1"
+	"github.com/rancher/wrangler/pkg/relatedresource"
+	core "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 func Register(
@@ -24,6 +28,59 @@ func Register(
 	serviceAccounts corev1.ServiceAccountController,
 	jobs batchv1.JobController,
 ) {
+	// watch configs and secrets
+	relatedresource.Watch(ctx, "state-config-secret-watch",
+		func(namespace, name string, obj runtime.Object) ([]relatedresource.Key, error) {
+			var statesFound []string
+			var result []relatedresource.Key
+
+			stateList, err := states.List(namespace, metaV1.ListOptions{})
+			if err != nil {
+				return nil, err
+			}
+			for _, state := range stateList.Items {
+				if _, ok := obj.(*core.ConfigMap); ok {
+					for _, envCm := range state.Spec.Variables.EnvConfigName {
+						config := obj.(*core.ConfigMap)
+						if envCm == config.Name {
+							statesFound = append(statesFound, state.Name)
+						}
+					}
+					for _, configmap := range state.Spec.Variables.ConfigNames {
+						config := obj.(*core.ConfigMap)
+						if configmap == config.Name {
+							statesFound = append(statesFound, state.Name)
+						}
+					}
+				}
+				if _, ok := obj.(*core.Secret); ok {
+					for _, envSecret := range state.Spec.Variables.EnvSecretNames {
+						secret := obj.(*core.Secret)
+						if envSecret == secret.Name {
+							statesFound = append(statesFound, state.Name)
+						}
+					}
+					for _, rSecret := range state.Spec.Variables.SecretNames {
+						secret := obj.(*core.Secret)
+						if rSecret == secret.Name {
+							statesFound = append(statesFound, state.Name)
+						}
+					}
+				}
+
+			}
+			if len(statesFound) > 0 {
+				for _, foundState := range statesFound {
+					result = append(result, relatedresource.NewKey(namespace, foundState))
+				}
+				return result, nil
+			}
+			return nil, nil
+		},
+		states,
+		configMaps,
+		secrets)
+
 	stateHandler := state.NewHandler(
 		ctx,
 		modules,
