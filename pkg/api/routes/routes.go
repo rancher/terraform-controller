ed6082ce13c57a3de1d3d30b2c27ad475f5fd334
Dinar Valeev <dinar.valeev@absa.africa>
2021-03-24 16:16:54 +0100

    Initial remote state dowload
    Signed-off-by: Dinar Valeev <dinar.valeev@absa.africa>

--- pkg/api/routes/routes.go
+++ pkg/api/routes/routes.go
@@ -1,10 +1,19 @@
 package routes
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+
 	"github.com/gin-gonic/gin"
 	"github.com/google/jsonapi"
 	"github.com/hashicorp/go-tfe"
 	"github.com/rancher/terraform-controller/pkg/types"
+
+	//"github.com/sirupsen/logrus"
+	"compress/gzip"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var cs *types.Controllers
@@ -15,6 +24,10 @@ func Register(r *gin.Engine, controllers *types.Controllers) error {
 	r.GET("/.well-known/terraform.json", discovery)
 	r.GET("/api/v2/organizations/:org/entitlement-set", entitlement)
 	r.GET("/api/v2/organizations/:org/workspaces/:workspace", workspace)
+	r.GET("/api/v2/workspaces/:workspace/current-state-version", state)
+	r.GET("/api/v2/download/:workspace/state", stateDownload)
+	r.POST("/api/v2/workspaces/:workspace/actions/lock", stateLock)
+	r.POST("/api/v2/workspaces/:workspace/actions/unlock", stateUnlock)
 
 	return nil
 }
@@ -40,9 +53,49 @@ func discovery(c *gin.Context) {
 }
 
 func workspace(c *gin.Context) {
+	c.Header("Content-Type", jsonapi.MediaType)
 	workspace := &tfe.Workspace{}
 	workspace.Name = c.Param("workspace")
 	workspace.ID = "ws-123"
-	c.Header("Content-Type", jsonapi.MediaType)
 	jsonapi.MarshalPayload(c.Writer, workspace)
+}
+
+func stateLock(c *gin.Context) {
+	c.String(200, "OK")
+}
+func stateUnlock(c *gin.Context) {
+	c.String(200, "OK")
+}
+func state(c *gin.Context) {
+	c.Header("Content-Type", jsonapi.MediaType)
+	ws := c.Param("workspace")
+
+	stateVersion := &tfe.StateVersion{}
+	stateVersion.DownloadURL = fmt.Sprintf("download/%s/state", ws)
+	jsonapi.MarshalPayload(c.Writer, stateVersion)
+}
+
+func stateDownload(c *gin.Context) {
+	c.Header("Content-Type", jsonapi.MediaType)
+	//	ws := c.Param("workspace")
+	secret, _ := cs.Secret.Get("default", "tfstate-default-my-state", metav1.GetOptions{})
+	state, _ := gunzip(secret.Data["tfstate"])
+	c.String(200, state)
+}
+
+func gunzip(data []byte) (string, error) {
+	b := bytes.NewBuffer(data)
+	var r io.Reader
+	r, err := gzip.NewReader(b)
+	if err != nil {
+		return "", err
+	}
+
+	var resB bytes.Buffer
+	_, err = resB.ReadFrom(r)
+	if err != nil {
+		return "", err
+	}
+
+	return string(resB.Bytes()), nil
 }
